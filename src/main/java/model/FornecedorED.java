da3db11a0517314e4ddbcccc206d9472da5db3c7
sala04 <sala04@targettrust.local>
2018-09-17 11:35:12 -0300

    q m

--- src/main/java/model/FornecedorED.java
+++ src/main/java/model/FornecedorED.java
@@ -1,6 +1,6 @@
-package model;
+package mo
 
-public class FornecedorED extends PessoaED {
+public class FornecedorED extends  {
 
 	private String cnpj;
 
@@ -10,4 +10,49 @@ public class FornecedorED extends PessoaED {
 
 	private String contaBsncaria;
 
+	public FornecedorED() {
+	}
+
+	public FornecedorED(String cnpj, String nomeEmpresa, int percentualComissao, String contaBsncaria) {
+		this.cnpj = cnpj;
+		this.nomeEmpresa = nomeEmpresa;
+		this.percentualComissao = percentualComissao;
+		this.contaBsncaria = contaBsncaria;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+
+	public int getPercentualComissao() {
+		return percentualComissao;
+	}
+
+	public void setPercentualComissao(int percentualComissao) {
+		this.percentualComissao = percentualComissao;
+	}
+
+	public String getContaBsncaria() {
+		return contaBsncaria;
+	}
+
+	public void setContaBsncaria(String contaBsncaria) {
+		this.contaBsncaria = contaBsncaria;
+	}
 }
--- src/main/java/model/PessoaED.java
+++ src/main/java/model/PessoaED.java
@@ -14,4 +14,60 @@ public class PessoaED {
 
 	private TelefoneED telefoneED;
 
+	public PessoaED(String nome, Date dataNascimento, String email, Tipo_PessoaED tipo_PessoaED, EnderecoED enderecoED, TelefoneED telefoneED) {
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.email = email;
+		this.tipo_PessoaED = tipo_PessoaED;
+		this.enderecoED = enderecoED;
+		this.telefoneED = telefoneED;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Tipo_PessoaED getTipo_PessoaED() {
+		return tipo_PessoaED;
+	}
+
+	public void setTipo_PessoaED(Tipo_PessoaED tipo_PessoaED) {
+		this.tipo_PessoaED = tipo_PessoaED;
+	}
+
+	public EnderecoED getEnderecoED() {
+		return enderecoED;
+	}
+
+	public void setEnderecoED(EnderecoED enderecoED) {
+		this.enderecoED = enderecoED;
+	}
+
+	public TelefoneED getTelefoneED() {
+		return telefoneED;
+	}
+
+	public void setTelefoneED(TelefoneED telefoneED) {
+		this.telefoneED = telefoneED;
+	}
 }
