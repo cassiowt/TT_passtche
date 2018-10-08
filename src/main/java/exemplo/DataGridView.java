16d4309e49e6000de05d252a0d1ec4cc3f9f981b
Cassio Trindade <cassio.trindade@pucrs.br>
2018-10-08 10:56:22 -0300

    iniciado exemplo prime

---
+++ src/main/java/exemplo/DataGridView.java
@@ -0,0 +1,42 @@
+package exemplo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean
+@ViewScoped
+public class DataGridView implements Serializable {
+
+    private List<Car> cars;
+
+    private Car selectedCar;
+
+    @ManagedProperty("#{carService}")
+    private CarService service;
+
+    @PostConstruct
+    public void init() {
+        cars = service.createCars(48);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setService(CarService service) {
+        this.service = service;
+    }
+
+    public Car getSelectedCar() {
+        return selectedCar;
+    }
+
+    public void setSelectedCar(Car selectedCar) {
+        this.selectedCar = selectedCar;
+    }
+}
