72fc1c9b0e8e82afebe5ffc62b1411b066767c3f
Alessandro <alessandro.alesilva@gmail.com>
2018-10-22 13:10:25 -0200

    ajustes fornecedor

--- src/main/java/mb/FornecedorMB.java
+++ src/main/java/mb/FornecedorMB.java
@@ -1,10 +1,14 @@
 package mb;
 
+import model.ClienteED;
+import model.TelefoneED;
 import rn.FornecedorRN;
 import model.FornecedorED;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.util.List;
+import java.util.ArrayList;
 
 @ManagedBean
 @SessionScoped
@@ -12,16 +16,20 @@ public class FornecedorMB {
 
     private FornecedorED fornecedor;
     private FornecedorRN fornecedorRN;
+    private TelefoneED telefone;
+    private List<TelefoneED> telefones;
 
     public FornecedorMB() {
         fornecedor = new FornecedorED();
+        fornecedorRN = new FornecedorRN();
+        telefones = new ArrayList<TelefoneED>();
     }
 
-    public FornecedorED getFornecedorED() {
+    public FornecedorED getFornecedor() {
         return fornecedor;
     }
 
-    public void setFornecedorED(FornecedorED fornecedor){
+    public void setFornecedor(FornecedorED fornecedor){
         this.fornecedor = fornecedor;
     }
 
@@ -32,6 +40,24 @@ public class FornecedorMB {
     public String saveFornecedor() {
 
         System.out.println(fornecedor);
+        fornecedorRN.createFornecedor(fornecedor);
         return null;
+    }
+    public List<FornecedorED> getFornecedores(){
+        List<FornecedorED> cs  = fornecedorRN.findAllFornecedor();
+        System.out.println(cs);
+        return cs;
+    }
+
+    public TelefoneED getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(TelefoneED telefone) {
+        this.telefone = telefone;
+    }
+
+    public void addTelefone(TelefoneED telefone) {
+        telefones.add(telefone);
     }
 }
